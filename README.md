2132ba23abe9ca756b11f4f783e816b14e0f85b5
kengz <kengzwl@gmail.com>
2022-06-20 10:19:07 -0400

    chore(setup): setup project

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # lean-dl-example
+
 Example of a lean deep learning project with a config-driven approach.
+
+## Installation
+
+[Install Conda](https://docs.conda.io/projects/conda/en/latest/user-guide/install/index.html) if you haven't already.
+
+Create a Conda environment and install dependencies. This example uses:
+
+- [Hydra](https://hydra.cc) for composable config
+- [feature_transform](https://github.com/kengz/feature_transform) for config-driven feature transformation
+- [torcharc](https://github.com/kengz/torcharc) for config-driven model building
+- [PyTorch Lightning](https://www.pytorchlightning.ai) for research-focused development
+- [Optuna](https://hydra.cc/docs/plugins/optuna_sweeper/) for hyperparameter tuning
+
+```bash
+conda create -n dl python=3.10.4 -y
+conda activate dl
+pip install -r requirements.txt
+```
+
+## Usage
+
+Inspect/modify the Hydra config at `config.yaml`. Then run:
+
+```bash
+python dl/train.py
+
+# to change configs
+python dl/train.py datamodule.batch_size=64 model.hidden_size=128
+
+# to resume (just the the resume config)
+python dl/train.py datamodule.batch_size=64 model.hidden_size=128 resume=true
+```
+
+This run training with validation at epoch-end, and test when training is done. Metrics will be logged from torchmetrics.
---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+import sys
+from setuptools import setup
+from setuptools.command.test import test
+
+
+test_args = [
+    '--verbose',
+    '--capture=sys',
+    '--log-level=INFO',
+    '--log-file-level=INFO',
+    '--cov-report=html',
+    '--cov-report=term',
+    '--cov=dl',
+    'test',
+]
+
+
+class PyTest(test):
+    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
+
+    def initialize_options(self):
+        os.environ['ENV'] = 'test'
+        test.initialize_options(self)
+        self.pytest_args = test_args
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='dl',
+    version='0.0.0',
+    description='Example of a lean deep learning project with a config-driven approach.',
+    long_description='https://github.com/kengz/lean-dl-example',
+    keywords='dl',
+    url='https://github.com/kengz/lean-dl-example',
+    author='kengz',
+    author_email='kengzwl@gmail.com',
+    packages=['dl'],
+    install_requires=[],
+    zip_safe=False,
+    include_package_data=True,
+    dependency_links=[],
+    extras_require={
+        'dev': [],
+        'docs': [],
+        'testing': [],
+    },
+    classifiers=[],
+    test_suite='test',
+    cmdclass={'test': PyTest},
+)
