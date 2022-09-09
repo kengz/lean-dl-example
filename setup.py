fcdb39b906c7b75a6060d2d5e6803082d2f8ae8f
kengz <kengzwl@gmail.com>
2022-09-09 09:55:24 -0400

    feat(conda): switch to conda

--- setup.py
+++ setup.py
@@ -1,34 +1,4 @@
-import os
-import sys
 from setuptools import setup
-from setuptools.command.test import test
-
-
-test_args = [
-    '--verbose',
-    '-W ignore',  # ignore warnings
-    '--capture=sys',
-    '--log-level=INFO',
-    '--log-file-level=INFO',
-    '--cov-report=html',
-    '--cov-report=term',
-    '--cov=dl',
-    'test',
-]
-
-
-class PyTest(test):
-    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
-
-    def initialize_options(self):
-        os.environ['ENV'] = 'test'
-        test.initialize_options(self)
-        self.pytest_args = test_args
-
-    def run_tests(self):
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
 
 
 setup(
@@ -51,12 +21,4 @@ setup(
         'testing': [],
     },
     classifiers=[],
-    tests_require=[
-        'autopep8',
-        'flake8',
-        'pytest',
-        'pytest-cov',
-    ],
-    test_suite='test',
-    cmdclass={'test': PyTest},
 )
