37a867b486527c126a9019c8c0736a6c72fa5ad5
kengz <kengzwl@gmail.com>
2024-02-10 21:58:55 -0500

    test: update unit test

--- test/test_datamodule.py
+++ test/test_datamodule.py
@@ -1,9 +1,8 @@
-from conftest import get_cfg
+from dl import cfg
 from dl.datamodule import DLDataModule
 
 
 def test_prepare_data():
-    cfg = get_cfg()
     dm = DLDataModule(cfg)
     dm.prepare_data()
     dm.setup('fit')
--- test/test_train.py
+++ test/test_train.py
@@ -1,4 +1,4 @@
-from conftest import get_cfg
+from dl import get_cfg
 from dl.train import main
 
 
